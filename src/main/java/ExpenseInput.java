e2ef2c7d4dcccba3d746012dc84553c32f0df2e9
omrilevia <omrilevia@gmail.com>
2021-11-07 20:47:04 -0800

    regex pattern matching for input validation

--- src/main/java/ExpenseInput.java
+++ src/main/java/ExpenseInput.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
-
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 public class ExpenseInput implements Receiver {
-
+    private Pattern pattern;
+    public ExpenseInput(){
+        pattern = Pattern.compile("^(0?[1-9]|[12][0-9]|3[01])-[0-9]+-[0-9]+\\s[0-9]+\\.[0-9]+\\s[a-zA-Z]+$", Pattern.CASE_INSENSITIVE);
+    }
 
     public void doAction() {
         System.out.println("Enter each expense in the format '[MM-DD-YYYY] xx.xx [tag]', enter q to quit");
@@ -25,7 +29,9 @@ public class ExpenseInput implements Receiver {
     }
 
     public boolean validateInput(String input){
-        return true;
+        final Matcher matcher = pattern.matcher(input);
+        // Use results...
+        return matcher.matches();
     }
 
 
